8e4caea900d5fe8faccbd5a85486e4c5c072faa8
AndreySevostyanov <notasimplehuski@gmail.com>
2022-04-07 07:39:07 +0000

    Added some funcs

--- pack_tut/src/expack/example.py
+++ pack_tut/src/expack/example.py
@@ -1,2 +1,10 @@
+"""Some basic funcs"""
 def add_one(number):
+    """Entered number + 1"""
     return number + 1
+def dividebytwo(number):
+    """Divides entered number by 2"""
+    return number/2
+def helloname(name):
+    """Says hello + entered name"""
+    return "Hello"+name
